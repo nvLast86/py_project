d30b438b7054723ccc4f97c66b6e1ce27a8ae0ac
nvLast86 <n.lastenko@gmail.com>
2023-02-03 00:06:36 +0600

    Subtraction function has been imported for subtraction of two user's numbers

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ first_number = input('Введите первое число\n')
 second_number = input('Введите второе число\n')
 
 summ = funcs.get_sum(first_number, second_number)
+subtr = funcs.get_subtraction(first_number, second_number)
+
 
 
 
