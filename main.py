b9f827c919ff3fc445ed547de64041d80bb2520f
nvLast86 <n.lastenko@gmail.com>
2023-02-03 00:05:24 +0600

    Summary function has been imported for summary of two user's numbers

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import funcs
 
 
 first_number = input('Введите первое число\n')
-second_number = input('Введите второе число')
+second_number = input('Введите второе число\n')
+
+summ = funcs.get_sum(first_number, second_number)
+
+
+
